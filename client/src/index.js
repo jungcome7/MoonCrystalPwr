6e58aa845fe70e00e821c8b63824c723f179e8b4
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2020-08-31 02:26:17 +0900

    CORS 문제로 FETCH 안되는 상황

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,4 @@
 const app = document.querySelector('#App');
-
 const template = `
     <div>this is afreeca</div>
     
@@ -8,3 +7,13 @@ const template = `
 app.style.height = '100px';
 app.style.backgroundColor = 'grey';
 app.innerHTML = template;
+
+
+const fetchData = async () => {
+    const res = await fetch('http://localhost:3000/user')
+    console.log(res)
+    const result = await res.json();
+    console.log(result)
+}
+
+app.addEventListener('click', fetchData);
--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const mysql = require('mysql2/promise');
+const cors = require('cors');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -17,11 +18,17 @@ const getAllUser = async (req, res, next) => {
   const result = await connection.query('SELECT * FROM user');
   connection.release();
   res.status(200).json(result[0]);
-} 
+};
 
 const app = express();
 
 app.use(logger('dev'));
+
+const corsOptions = {
+  origin: 'http://localhost:3000', // 허락하고자 하는 요청 주소
+  credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
+};
+app.use(cors(corsOptions));
 
 app.get('/', getAllUser);
 
