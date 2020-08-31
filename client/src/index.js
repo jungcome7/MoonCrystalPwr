33480ff86ea9f2d478db3499004bc0869d2b36d6
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2020-08-31 16:37:56 +0900

    일단 여기까지

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ const template = `
 app.style.height = '100px';
 app.style.backgroundColor = 'grey';
 app.innerHTML = template;
+app.style.cursor = 'pointer';
 
 const fetchData = async () => {
   const res = await fetch('http://localhost:3000/');
@@ -16,7 +17,9 @@ const fetchData = async () => {
 
 const clickHandler = async (e) => {
   const data = await fetchData();
-  e.target.innerHTML=(`id: ${data[0].id}, name: ${data[0].name}, email: ${data[0].email}`)
+  !e.target.innerHTML
+    ? (e.target.innerHTML = `id: ${data[0].id}, name: ${data[0].name}, email: ${data[0].email}`)
+    : (e.target.innerHTML = '');
 };
 
 app.addEventListener('click', (e) => clickHandler(e));
