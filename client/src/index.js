09553c2a64be662ddfc4fe1f7ba30ece33d9ecb8
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2020-09-03 19:54:06 +0900

    마크업 작업을 위한 세팅

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,25 @@
 const app = document.querySelector('#App');
-const template = `
-    <div>this is afreeca</div>
+// const template = `
+//     <div>this is afreeca</div>
     
-`;
+// `;
 
-app.style.height = '100px';
-app.style.backgroundColor = 'grey';
-app.innerHTML = template;
-app.style.cursor = 'pointer';
+// app.style.height = '100px';
+// app.style.backgroundColor = 'grey';
+// app.innerHTML = template;
+// app.style.cursor = 'pointer';
 
-const fetchData = async () => {
-  const res = await fetch('http://localhost:3000/');
-  const result = await res.json();
-  return result;
-};
+// const fetchData = async () => {
+//   const res = await fetch('http://localhost:3000/');
+//   const result = await res.json();
+//   return result;
+// };
 
-const clickHandler = async (e) => {
-  const data = await fetchData();
-  !e.target.innerHTML
-    ? (e.target.innerHTML = `id: ${data[0].id}, name: ${data[0].name}, email: ${data[0].email}`)
-    : (e.target.innerHTML = '');
-};
+// const clickHandler = async (e) => {
+//   const data = await fetchData();
+//   !e.target.innerHTML
+//     ? (e.target.innerHTML = `id: ${data[0].id}, name: ${data[0].name}, email: ${data[0].email}`)
+//     : (e.target.innerHTML = '');
+// };
 
 app.addEventListener('click', (e) => clickHandler(e));
