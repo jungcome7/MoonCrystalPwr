2cfadeacc61f372fa8cf070475b6a229249a5c12
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2020-09-04 13:27:44 +0900

    create SideBar module

---
+++ client/src/component/SideBar/SideBar.js
@@ -0,0 +1,6 @@
+import template from './template';
+
+export function SideBar() {
+  document.body.innerHTML = template;
+}
+
---
+++ client/src/component/SideBar/template.js
@@ -0,0 +1,20 @@
+const template = `
+<div class="sidebar">
+  <div class="logo">logo</div>
+  <div class="main-category">
+    <div class="main-category-all">MainCategoryAll</div>
+    <div class="main-category-1">MainCategory1</div>
+    <div class="main-category-2">MainCategory2</div>
+    <div class="main-category-3">MainCategory3</div>
+    <div class="main-category-4">MainCategory4</div>
+    <div class="main-category-5">MainCategory5</div>
+    <div class="main-category-6">MainCategory6</div>
+    <div class="main-category-7">MainCategory7</div>
+    <div class="main-category-8">MainCategory8</div>
+    <div class="main-category-9">MainCategory9</div>
+  </div>
+  <input type="text" placeholder="search" />
+</div>
+`;
+
+export default template;
