e728bd99d64cf35154d856a4981bacbc6e9c1035
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2020-09-05 20:08:32 +0900

    modify components design

--- client/src/component-depth1/SideBar/template.js
+++ client/src/component-depth1/SideBar/template.js
@@ -1,22 +1,4 @@
 const template = `
-<div class="sidebar">
-  <div class="logo">logo</div>
-  <div class="main-category">
-    <div class="main-category-tab main-category-all">MainCategoryAll</div>
-    <div class="main-category-tab main-category-1">MainCategory1</div>
-    <div class="main-category-tab main-category-2">MainCategory2</div>
-    <div class="main-category-tab main-category-3">MainCategory3</div>
-    <div class="main-category-tab main-category-4">MainCategory4</div>
-    <div class="main-category-tab main-category-5">MainCategory5</div>
-    <div class="main-category-tab main-category-6">MainCategory6</div>
-    <div class="main-category-tab main-category-7">MainCategory7</div>
-    <div class="main-category-tab main-category-8">MainCategory8</div>
-    <div class="main-category-tab main-category-9">MainCategory9</div>
-  </div>
-  <div class="search-form">
-    <input type="text" placeholder="search" />
-  </div>
-</div>
 `;
 
 export default template;
