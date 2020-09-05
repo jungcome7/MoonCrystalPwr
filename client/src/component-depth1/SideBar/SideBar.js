fcc5315984d07a5b5a30a3855131aef1a5ac06cb
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2020-09-05 19:22:06 +0900

    add several MainCategoryTab instances

--- client/src/component-depth1/SideBar/SideBar.js
+++ client/src/component-depth1/SideBar/SideBar.js
@@ -5,10 +5,15 @@ import { MainCategoryTab } from '../../component-depth2/MainCategoryTab'
 function SideBar() {
     const sidebarContainer = document.querySelector('.sidebar-container');
     sidebarContainer.appendChild(new MainCategoryTab('logo'));
+    sidebarContainer.appendChild(new MainCategoryTab('catAll'));
     sidebarContainer.appendChild(new MainCategoryTab('cat1'));
     sidebarContainer.appendChild(new MainCategoryTab('cat2'));
     sidebarContainer.appendChild(new MainCategoryTab('cat3'));
-    sidebarContainer.appendChild(new MainCategoryTab('cat4'));
+    sidebarContainer.appendChild(new MainCategoryTab('cat5'));
+    sidebarContainer.appendChild(new MainCategoryTab('cat6'));
+    sidebarContainer.appendChild(new MainCategoryTab('cat7'));
+    sidebarContainer.appendChild(new MainCategoryTab('cat8'));
+    sidebarContainer.appendChild(new MainCategoryTab('cat9'));
 }
 
 export default SideBar;
